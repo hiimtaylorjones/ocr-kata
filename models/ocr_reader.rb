e131cc968e4a8899c04ba6080c2438935eb768f7
hiimtaylorjones <taylorjonessoftware@gmail.com>
2017-04-05 22:55:47 -0400

    [tests] - filling out OCR reader test

--- models/ocr_reader.rb
+++ models/ocr_reader.rb
@@ -1,2 +1,10 @@
+# frozen_string_literal: true
 class OcrReader
+  attr_reader :data
+  def initialize(file_path)
+    @data = File.read(file_path)
+  end
+
+  def process
+  end
 end
--- tests/models/ocr_reader_test.rb
+++ tests/models/ocr_reader_test.rb
@@ -2,8 +2,13 @@ require './rspec_manifest.rb'
 
 RSpec.describe OcrReader do
   context 'with a valid input file' do
+    it 'should have a method to access file data' do
+      reader = OcrReader.new('../../stories/1.txt')
+      expect(reader.data).to_not eq(nil)
+    end
+
     it 'should be able to take in a file line and return an OcrResult' do
-      reader = OcrReader.new('stories/1.txt')
+      reader = OcrReader.new('../../stories/1.txt')
       ocr_result = OcrResult.new([1, 2, 3, 4, 5, 6, 7, 8, 9])
       processed = reader.process
       expect(processed).to eq(true)
