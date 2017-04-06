d83e0e104bb6d7bf750eb139b95a23194ca5d580
hiimtaylorjones <taylorjonessoftware@gmail.com>
2017-04-05 22:35:41 -0400

    [tests] - filling out ocr reader test

--- tests/models/ocr_reader_test.rb
+++ tests/models/ocr_reader_test.rb
@@ -1,9 +1,13 @@
 require './rspec_manifest.rb'
 
 RSpec.describe OcrReader do
-  it "should have an initializer"
-
-  context "with a valid input file" do
-    it "should be able to take in a file line and return an OcrResult"
+  context 'with a valid input file' do
+    it 'should be able to take in a file line and return an OcrResult' do
+      reader = OcrReader.new('stories/1.txt')
+      ocr_result = OcrResult.new([[1], [2], [3], [4], [5], [6], [7], [8], [9]])
+      processed = reader.process
+      expect(processed).to eq(true)
+      expect(reader.result).to eq(ocr_result)
+    end
   end
 end
